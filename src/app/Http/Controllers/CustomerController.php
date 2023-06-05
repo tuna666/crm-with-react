69b1e7d1f72808861b88da7677bb560dc5e33ece
tuna <ev0.kpx@gmail.com>
2023-06-05 18:03:50 +0900

    indexアクション作成

--- src/app/Http/Controllers/CustomerController.php
+++ src/app/Http/Controllers/CustomerController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CustomerController extends Controller
 {
-    //
+    public function index()
+    {
+        // Customerモデルを使って、全件取得する
+        $customers = Customer::all();
+
+        // 取得した結果をinertia.jsを使ってreactのビューに渡す
+        return inertia::render('Customer/Index', [
+            'customers' => $customers,
+        ]);
+    }
 }
