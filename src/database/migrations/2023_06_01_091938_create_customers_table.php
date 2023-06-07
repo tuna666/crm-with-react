5ad51c9e096776a7818af635868bd05e517b2458
tuna <ev0.kpx@gmail.com>
2023-06-07 17:58:48 +0900

    氏名を必須に変更

--- src/database/migrations/2023_06_01_091938_create_customers_table.php
+++ src/database/migrations/2023_06_01_091938_create_customers_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->string('last_name')->nullable();
-            $table->string('first_name')->nullable();
+            $table->string('last_name');
+            $table->string('first_name');
             $table->string('email')->nullable();
             $table->string('phone')->nullable();
             $table->text('memo')->nullable();
